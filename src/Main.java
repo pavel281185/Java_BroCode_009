9a73378a2b9821893975274660d8f88669d31720
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 14:06:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // if statement = performs a block of code if it is condition evaluates to be true
+
+        int age = 16;
+
+        if (age >= 75){
+            System.out.println("OK, Boomer!");
+        } else if (age >= 18) {
+            System.out.println("You are an adult!");
+        } else if (age >= 13) {
+            System.out.println("You are a teenager!");
+        } else {
+            System.out.println("You are not an adult!");
+        }
+    }
+}
